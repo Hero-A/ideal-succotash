eb0163f6d6f656279f9225464945fdb789b1df54
Hero-A <92825909+Hero-A@users.noreply.github.com>
2021-12-30 11:18:12 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -16,9 +20,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
           console.log(results);
+          leftWristX = results[0].pose.leftWrist.x;
+          rightWristX = results[0].pose.rightWrist.x;
+          difference = floor(leftWristX - rightWristX);
     }
 }
 
 function draw(){
     background("grey")
+    textSize(difference);
+    fill('green');
+    text("Vihaan", 50, 400);
 }
